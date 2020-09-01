Fixed bug where files in the jacobian were not sorted. Also fixed bug that incorrectly set the wnum bounds when running lblrtm

TROPoe.py
@@ -975,8 +975,10 @@ for i in range(len(aeri['secs'])):                        # { loop_i
 
     # Select nice round numbers to use as the wavenumber limits for
     # the LBLRTM calc, but remember that I need to pad by 50 cm-1 for FSCAN
-    lblwnum1 = np.int(np.min(wnum)-60)/100 * 100
-    lblwnum2 = (np.int(np.max(wnum)+60)/100+1)*100
+#     lblwnum1 = np.int(np.min(wnum)-60)/100 * 100
+#     lblwnum2 = (np.int(np.max(wnum)+60)/100+1)*100
+    lblwnum1 = np.int((np.min(wnum)-60)/100) * 100
+    lblwnum2 = (np.int((np.max(wnum)+60)/100)+1)*100
     continue_next_sample = 0          # A flag used to watch for bad jacobian calcs
 
     while ((itern <= vip['max_iterations']) & (converged == 0)):        # { While loop over iter