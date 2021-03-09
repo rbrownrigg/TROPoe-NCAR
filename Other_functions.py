Various changes related to fixing bugs in PBLH calculation

Other_functions.py
@@ -300,25 +300,33 @@ def inflate_prior_covariance(Sa, z, prior_t_ival, prior_t_iht, prior_q_ival,
 # even for radiosonde profiles)
 ################################################################################
 
-def compute_pblh(ht, t, p, nudge=0.5, minht=0.300):
-    theta = Calcs_Conversions.t2theta(t,np.zeros(len(t)),p)
-    foo = np.where(ht >= minht)[0]
-    if len(foo) <= 0:
-       sval = np.nanmean(theta[0:2])
-    else:
-       sval = np.nanmean([theta[foo[0]-1],theta[foo[0]]])
+def compute_pblh(ht, t, p, sigt, nudge=0.5, minht=0.300, maxht=6.0):
+    st = np.zeros_like(sigt)
+    st[0] = sigt[0]
+    theta = Calcs_Conversions.t2theta(t, np.zeros(len(t)), p)
+    foo = np.where(ht >= minht)
+    sval = theta[0]
     sval += nudge
-    foo = np.where((sval <= theta) & (ht >= minht))[0]
+    foo = np.where((sval < theta) & (ht >= minht))[0]
     if len(foo) == 0:
-        return minht
+        pblh = minht
     elif foo[0] == 0:
         pblh = ht[0]
     else:
-        tmpz = np.array([ht[foo[0]-1], ht[foo[0]]])
-        tmpt = np.array([theta[foo[0]-1], theta[foo[0]]])
-        pblh = np.interp(sval, tmpt, tmpz)
+        tmpz = ht[foo[0]-1:foo[0]+1]
+        tmpt = ht[foo[0]-1:foo[0]+1]
+
+        if np.abs(tmpt[0]-tmpt[1]) < 0.001:
+            pblh = ht[foo[0]]
+        else:
+            pblh = np.interp(sval, tmpt, tmpz)
+
     if pblh < minht:
-        pblh = -999.
+        pblh = minht
+
+    if pblh > maxht:
+        pblh = maxht
+
     return pblh
 
 ################################################################################

Output_Functions.py
@@ -562,11 +562,12 @@ def write_output(vip, ext_prof, mod_prof, ext_tseries, globatt, xret, prior,
     if len(dindex_name) != len(dindex_units):
         print('Error in write_output: there is a dimension mismatch in the derived indices dindex_')
         return success, nfilename
+
     for i in range(npts):
         # Extract out the temperature and water vapor profiles
-        pp = np.copy(xret[i]['p'])
-        tt = np.copy(xret[i]['Xn'][0:nht])
-        ww = np.copy(xret[i]['Xn'][nht:2*nht])
+        pp = np.copy(xret[i+fsample]['p'])
+        tt = np.copy(xret[i+fsample]['Xn'][0:nht])
+        ww = np.copy(xret[i+fsample]['Xn'][nht:2*nht])
         
         # Extract out the apriori temperature and water vapor profiles
         ta = np.copy(prior['Xa'][0:nht])
@@ -575,6 +576,7 @@ def write_output(vip, ext_prof, mod_prof, ext_tseries, globatt, xret, prior,
         # Extract out the posterior covariance matrix
         Sop_tmp = np.copy(xret[i]['Sop'])
         Sop_tmp = Sop_tmp[0:2*nht,0:2*nht]
+        sig_t = np.sqrt(np.diag(Sop_tmp))[0:nht]
         
         # Perform SVD of posterior covariance matrix
         # Note that in order to follow the logic of the original IDL code I
@@ -600,9 +602,10 @@ def write_output(vip, ext_prof, mod_prof, ext_tseries, globatt, xret, prior,
             
             elif dindex_name[ii] == 'pblh':
                 minht = vip['min_PBL_height']
-                indices[ii,i] = Other_functions.compute_pblh(zz, tt, pp, minht = minht)
+                maxht = vip['max_PBL_height']
+                indices[ii,i] = Other_functions.compute_pblh(zz, tt, pp, sig_t, minht=minht, maxht=maxht)
                 for j in range(num_mc):
-                    tmp[j] = Other_functions.compute_pblh(zz,tprofs[:,j], pp, minht = minht)
+                    tmp[j] = Other_functions.compute_pblh(zz, tprofs[:, j], pp, sig_t, minht=minht, maxht=maxht)
                 foo = np.where(tmp > 0)[0]
                 if (len(foo) > 1) & (indices[ii,i] > 0):
                     sigma_indices[ii,i] = np.nanstd(indices[ii,i] - tmp[foo])

TROPoe.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.41'
+__version__ = '0.2.44'
 
 import os
 import sys
@@ -1008,7 +1008,12 @@ for i in range(len(aeri['secs'])):                        # { loop_i
         # then set that here. First, compute the current estimate of the PBL height,
         # then the coefficient and overwrite any current shape coefficient
 
-        pblh = Other_functions.compute_pblh(z, Xn[0:int(nX/2)], p, minht = vip['min_PBL_height'])
+        if itern == 0:
+            pblh = Other_functions.compute_pblh(z, Xn[0:int(nX/2)], p, np.sqrt(np.diag(Sa[0:int(nX/2), 0:int(nX/2)])),
+                                                minht=vip['min_PBL_height'], maxht=vip['max_PBL_height'])
+        else:
+            pblh = Other_functions.compute_pblh(z, Xn[0:int(nX / 2)], p, np.sqrt(np.diag(Sop[0:int(nX/2), 0:int(nX/2)])),
+                                                minht=vip['min_PBL_height'], maxht=vip['max_PBL_height'])
         coef = Other_functions.get_a2_pblh(pblh)           # Get the shape coef for this PBL height
         if ((vip['retrieve_co2'] == 1) & (vip['fix_co2_shape'] == 1)):
             Xn[nX+4+2] = coef