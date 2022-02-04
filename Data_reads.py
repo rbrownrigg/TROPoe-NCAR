Fixed external profiles reads, NCAR Jacobian read, possible time mismatch in grid_aeri function, and some syntax stuff

Data_reads.py
@@ -1,19 +1,15 @@
 import os, re
 import numpy as np
-import glob
 from netCDF4 import Dataset
 import calendar
 from datetime import datetime, timedelta
 
 import Other_functions
-import VIP_Databases_functions
 import Calcs_Conversions
-import Jacobian_Functions
-import Output_Functions
 
 
 ################################################################################
-# This file containts the following functions:
+# This file contains the following functions:
 # findfile()
 # read_all_data()
 # read_mwr()
@@ -1491,14 +1487,14 @@ def read_vceil(path, date, vceil_type, ret_secs, verbose):
             print('Reading in ASOS/AWOS')
 
         for i in range(len(udate)):
-            files = files + (findfile(path,'*(ceil,cbh)*' + udate[i] + '*.(cdf|nc)'))
+            files = files + (findfile(path, '*(ceil,cbh)*' + udate[i] + '*.(cdf|nc)'))
         if len(files) == 0:
             print('No CBH files found for this data')
             return err
 
         for i in range(len(files)):
             if verbose == 3:
-                print(' Reading in file ' + files(i))
+                print(' Reading in file ' + files[i])
 
             fid = Dataset(files[i],'r')
             bt = fid.variables['base_time'][:]
@@ -1747,7 +1743,7 @@ def grid_aeri(ch1, aerisum, avg_instant, hatchOpenSwitch, missingDataFlagSwitch,
                 if len(bar) == len(foo):
                     hatflag[i] = 1                             # Hatch was always open
                 else:
-                    np.where(ch1['hatchopen'][foo] == 0)[0]
+                    bar = np.where(ch1['hatchopen'][foo] == 0)[0]
                     if len(bar) == len(foo):
                       hatflag[i] = 0                           # Hatch was always closed
                     else:
@@ -1759,10 +1755,10 @@ def grid_aeri(ch1, aerisum, avg_instant, hatchOpenSwitch, missingDataFlagSwitch,
 
         # Get the summary data on this grid
         if avg_instant == 0:
-            foo = np.where((secs[i]-tavg*60./2. <= ch1['secs']) & (ch1['secs'] < secs[i]+tavg*60./2.))[0]
+            foo = np.where((secs[i]-tavg*60./2. <= aerisum['secs']) & (aerisum['secs'] < secs[i]+tavg*60./2.))[0]
         else:
             dell = np.abs(secs[i]-aerisum['secs'])
-            foo = np.where((secs[i]-tavg*60./2. <= ch1['secs']) & (ch1['secs'] < secs[i]+tavg*60./2.) &
+            foo = np.where((secs[i]-tavg*60./2. <= aerisum['secs']) & (aerisum['secs'] < secs[i]+tavg*60./2.) &
                                (dell == np.nanmin(dell)))[0]
 
         if len(foo) > 1:
@@ -2164,17 +2160,21 @@ def read_external_profile_data(date, ht, secs, tres, avg_instant,
                     swv = np.copy(swvx)
                 else:
                     qsecs = np.append(qsecs, bt+to)
-                    wv = np.append(wv,wvx, axis = 1)
-                    swv = np.append(swv,swvx, axis = 1)
+                    wv = np.append(wv,wvx, axis = 0)
+                    swv = np.append(swv,swvx, axis = 0)
                 nprof = len(qsecs)
 
             external['nQprof'] = len(qsecs)
+
 
         if external['nQprof'] > 0:
             zzq = np.copy(htx)
             wvmultiplier = 1.      # To scale the WV profiles to be reasonable order of magnitude
             wv = wv/wvmultiplier
             swv = wv/wvmultiplier
+
+        wv = wv.T
+        swv = swv.T
 
 
     # Read in the NCAR WV DIAL data (for 2014-2017 time period)
@@ -2208,8 +2208,8 @@ def read_external_profile_data(date, ht, secs, tres, avg_instant,
                     swv = np.copy(swvx)
                 else:
                     qsecs = np.append(qsecs,secsx)
-                    wv = np.append(wv, wvx, axis = 1)
-                    swv = np.append(swv, swvx, axis = 1)
+                    wv = np.append(wv, wvx, axis = 0)
+                    swv = np.append(swv, swvx, axis = 0)
                 external['nQprof'] = len(qsecs)
             zzq = zzq / 1000.           # Convert m AGL to km AGL
 
@@ -2314,10 +2314,13 @@ def read_external_profile_data(date, ht, secs, tres, avg_instant,
                     maxht = np.copy(mhtx)
                 else:
                     qsecs = np.append(qsecs,bt+to)
-                    wv = np.append(wv,wwx, axis = 1)
-                    swv = np.append(swv,ssx, axis = 1)
+                    wv = np.append(wv,wwx, axis = 0)
+                    swv = np.append(swv,ssx, axis = 0)
 
             external['nQprof'] = len(qsecs)
+
+            wv = wv.T
+            swv = swv.T
 
             # Set all of the points above the maximum height to -999.
 
@@ -2334,64 +2337,56 @@ def read_external_profile_data(date, ht, secs, tres, avg_instant,
 
     elif wv_prof_type == 6:
         if verbose >= 1:
-            print('Reading in NCAR WV DIAL data (after QC) to constrain the WV profile')
+            print('Reading in NCAR MPD data to constrain the WV profile')
 
         files = []
         for i in range(len(dates)):
-            files = files + (findfile(wv_prof_path,'*dial*' + str(int(dates[i])) + '*.(cdf|nc)'))
+            files = files + (findfile(wv_prof_path,'*mpd*' + str(int(dates[i])) + '*.(cdf|nc)'))
 
         if len(files) == 0:
             if verbose >= 1:
-                print('No NCAR WV DIAL found in this directory for this date')
+                print('No NCAR MPD data found in this directory for this date')
             external['nQprof'] = 0.0
         else:
             if verbose >= 2:
-                print('Reading ' + str(len(files)) + ' NCAR WV DIAL data files')
+                print('Reading ' + str(len(files)) + ' NCAR MPD data files')
             for i in range(len(files)):
                 fid = Dataset(files[i],'r')
-                bt = fid.variables['base_time'][0]
-                to = fid.variables['time_offset'][:]
-                zzq = fid.variables['height'][:]
-                wwx = fid.variables['qc_waterVapor'][:]
-                ssx = fid.variables['sigma_waterVapor'][:]
-                mhtx = fid.variables['maxht'][:]
+                bt = (datetime.strptime(files[i][-18:-10],'%Y%m%d') - datetime(1970,1,1)).total_seconds()
+                to = fid.variables['time'][:]
+                zzq = fid.variables['range'][:]
+                wwx = fid.variables['Absolute_Humidity'][:]
+                ssx = np.sqrt(fid.variables['Absolute_Humidity_variance'][:])
+                maskx = fid.variables['Absolute_Humidity_mask'][:]
                 fid.close()
 
                 if i == 0:
                     qsecs = bt+to
                     wv = np.copy(wwx)
                     swv = np.copy(ssx)
-                    maxht = np.copy(mhtx)
+                    mask = np.copy(maskx)
                 else:
                     qsecs = np.append(qsecs,bt+to)
-                    wv = np.append(wv,wwx, axis = 1)
-                    swv = np.append(swv,ssx, axis = 1)
-                    maxht = np.append(maxht,mhtx)
+                    wv = np.append(wv,wwx, axis = 0)
+                    swv = np.append(swv,ssx, axis = 0)
+                    maxht = np.append(mask,maskx,axis=0)
 
                 external['nQprof'] = len(qsecs)
 
             zzq = zzq / 1000.           # Convert m AGL to km AGL
 
-# TODO: Tyler -- please make sure the columns/rows are correct (logic below is assuming [height,time])
         wv = wv.T
         swv = swv.T
+        mask = mask.T
 
-            # Set all of the points above the maximum height to -999.
-        for i in range(len(qsecs)):
-          foo = np.where(zzq > maxht[i])[0]
-          if(len(foo) > 0):
-              wv[foo,i]  = -999.
-              swv[foo,i] = -999.
+        # Apply the mask to the data.
+        foo = np.where(mask == 1)
+        wv[foo] = np.nan
+        swv[foo] = np.nan
 
         if external['nQprof'] > 0:
             qunit = 'g/m3'
-            qtype = 'NCAR WV DIAL data'
-
-            # Set all WV values less than 0 to -999 also
-        foo = np.where(zzq < 0)
-        if(len(foo) > 0):
-            wv[foo]  = -999
-            swv[foo] = -999
+            qtype = 'NCAR MPD data'
 
     elif wv_prof_type == 99:
         if verbose >= 1:
@@ -3318,8 +3313,8 @@ def read_external_timeseries(date, secs, tres, avg_instant, sfc_temp_type,
                 w2 = Calcs_Conversions.rh2w(t-sigma_t, u/100., p)
                 u_plus = (u+sigma_u)/100.
                 u_minus = (u+sigma_u)/100.
-                u_plus[u_plus > 1] == 1
-                u_minus[u_minus < 0] == 0
+                u_plus[u_plus > 1] = 1
+                u_minus[u_minus < 0] = 0
                 w3 = Calcs_Conversions.rh2w(t, u_plus, p)
                 w4 = Calcs_Conversions.rh2w(t, u_minus, p)
 
@@ -3393,8 +3388,8 @@ def read_external_timeseries(date, secs, tres, avg_instant, sfc_temp_type,
                 w2 = Calcs_Conversions.rh2w(t-sigma_t, u/100., p)
                 u_plus = (u+sigma_u)/100.
                 u_minus = (u+sigma_u)/100.
-                u_plus[u_plus > 1] == 1
-                u_minus[u_minus < 0] == 0
+                u_plus[u_plus > 1] = 1
+                u_minus[u_minus < 0] = 0
                 w3 = Calcs_Conversions.rh2w(t, u_plus, p)
                 w4 = Calcs_Conversions.rh2w(t, u_minus, p)
 
@@ -3483,8 +3478,8 @@ def read_external_timeseries(date, secs, tres, avg_instant, sfc_temp_type,
                 w2 = Calcs_Conversions.rh2w(t-sigma_t, u/100., p)
                 u_plus = (u+sigma_u)/100.
                 u_minus = (u+sigma_u)/100.
-                u_plus[u_plus > 1] == 1
-                u_minus[u_minus < 0] == 0
+                u_plus[u_plus > 1] = 1
+                u_minus[u_minus < 0] = 0
                 w3 = Calcs_Conversions.rh2w(t, u_plus, p)
                 w4 = Calcs_Conversions.rh2w(t, u_minus, p)
 

Jacobian_Functions.py
@@ -33,7 +33,7 @@ import LBLRTM_Functions
 def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, tp5, tp3,
                     cbh, sspl, sspi, lblwnum1, lblwnum2, fixt, fixwv, doco2, doch4, don2o,
                     fixlcld, fixicld, fix_co2_shape, fix_ch4_shape, fix_n2o_shape,
-                    jac_maxht, awnum, adeltaod, forward_threshold, sfc_alt, extra_layers, stdatmos, 
+                    jac_maxht, awnum, adeltaod, forward_threshold, sfc_alt, extra_layers, stdatmos,
                     verbose, debug, doapodize):
 
     success = 0
@@ -83,9 +83,9 @@ def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, 
     if verbose >= 3:
         print('Making the LBLRTM runs for the Jacobian')
 
-    LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+    LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile = n2oprof,
-             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.1', 
+             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.1',
              v10=True, silent=True)
 
     command1 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -98,9 +98,9 @@ def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, 
     if fixt != 1:
         tpert = 1.0            # Additive perturbation of 1 K
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t+tpert, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t+tpert, w,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile = n2oprof,
-             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.2', 
+             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.2',
              v10=True, silent=True)
 
         command2 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -115,9 +115,9 @@ def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, 
     if fixwv != 1:
         h2opert = 0.99
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w*h2opert, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w*h2opert,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile = n2oprof,
-             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.3', 
+             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.3',
              v10=True, silent=True)
 
         command3 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -135,9 +135,9 @@ def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, 
         c0[0] += co2pert        #An additive perturbation [ppm]
         co2prof2 = Other_functions.trace_gas_prof(doco2, zz, c0)
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof2, ch4_profile=ch4prof, n2o_profile = n2oprof,
-             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.4', 
+             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.4',
              v10=True, silent=True)
 
         command4 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -155,9 +155,9 @@ def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, 
         c0[0] += ch4pert        #An additive perturbation [ppm]
         ch4prof2 = Other_functions.trace_gas_prof(doch4, zz, c0)
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof, ch4_profile=ch4prof2, n2o_profile = n2oprof,
-             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.5', 
+             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.5',
              v10=True, silent=True)
 
         command5 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -176,9 +176,9 @@ def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, 
         c0[0] += n2opert      #An additive perturbation [ppm]
         n2oprof2 = Other_functions.trace_gas_prof(don2o, zz, c0)
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile = n2oprof2,
-             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.6', 
+             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.6',
              v10=True, silent=True)
 
         command6 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -812,9 +812,9 @@ def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, 
         if verbose >= 3:
             print('Forward model F(Xn) using LBLRTM and assuming no clouds')
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile = n2oprof,
-             mlayers=mlayerz, wnum1=lblwnum1-100, wnum2=lblwnum2+100, tape5=tp5+'.99', 
+             mlayers=mlayerz, wnum1=lblwnum1-100, wnum2=lblwnum2+100, tape5=tp5+'.99',
              v10=True, silent=True)
 
         command99 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -911,7 +911,7 @@ def compute_jacobian_deltaod(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, 
 def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos, tp5, tp3,
                     cbh, sspl, sspi, lblwnum1, lblwnum2, fixt, fixwv, doco2, doch4, don2o,
                     fixlcld, fixicld, fix_co2_shape, fix_ch4_shape, fix_n2o_shape,
-                    jac_maxht, awnum, forward_threshold, sfc_alt, extra_layers, 
+                    jac_maxht, awnum, forward_threshold, sfc_alt, extra_layers,
                     stdatmos, npts_per_wnum,
                     verbose, debug, doapodize):
 
@@ -965,9 +965,9 @@ def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos,
     if verbose >= 3:
         print('Making the LBLRTM runs for the Jacobian')
 
-    LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+    LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile=n2oprof,
-             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.1', 
+             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.1',
              v10=True, silent=True)
 
     command1 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -980,9 +980,9 @@ def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos,
     if fixt != 1:
         tpert = 1.0            # Additive perturbation of 1 K
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t+tpert, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t+tpert, w,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile=n2oprof,
-             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.2', 
+             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.2',
              v10=True, silent=True)
 
         command2 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -997,9 +997,9 @@ def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos,
     if fixwv != 1:
         h2opert = 0.99
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w*h2opert, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w*h2opert,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile=n2oprof,
-             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.3', 
+             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.3',
              v10=True, silent=True)
 
         command3 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -1017,9 +1017,9 @@ def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos,
         c0[0] += co2pert        #An additive perturbation [ppm]
         co2prof2 = Other_functions.trace_gas_prof(doco2, zz, c0)
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof2, ch4_profile=ch4prof, n2o_profile=n2oprof,
-             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.4', 
+             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.4',
              v10=True, silent=True)
 
         command4 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -1037,9 +1037,9 @@ def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos,
         c0[0] += ch4pert        #An additive perturbation [ppm]
         ch4prof2 = Other_functions.trace_gas_prof(doch4, zz, c0)
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof, ch4_profile=ch4prof2, n2o_profile=n2oprof,
-             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.5', 
+             od_only = 1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.5',
              v10=True, silent=True)
 
         command5 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -1057,9 +1057,9 @@ def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos,
         c0[0] += n2opert      #An additive perturbation [ppm]
         n2oprof2 = Other_functions.trace_gas_prof(don2o, zz, c0)
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile=n2oprof2,
-             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.6', 
+             od_only=1, mlayers=mlayerz, wnum1=lblwnum1, wnum2=lblwnum2, tape5=tp5+'.6',
              v10=True, silent=True)
 
         command6 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -1288,7 +1288,7 @@ def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos,
 
                 t0 = np.copy(mlayert)
                 t0[kk] += tpert
-                radc1 = Other_functions.radxfer(wnum, t0, gasod)  
+                radc1 = Other_functions.radxfer(wnum, t0, gasod)
                 radc1 += cldrefrad
 
                 tmp = Other_functions.convolve_to_aeri(wnum, radc1)
@@ -1697,9 +1697,9 @@ def compute_jacobian_interpol(X, p, zz, lblhome, lbldir, lblroot, lbl_std_atmos,
         if verbose >= 3:
             print('Forward model F(Xn) using LBLRTM and assuming no clouds')
 
-        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w, 
+        LBLRTM_Functions.rundecker(3, lbl_std_atmos, zz+sfcz, p, t, w,
              co2_profile=co2prof, ch4_profile=ch4prof, n2o_profile=n2oprof,
-             mlayers=mlayerz, wnum1=lblwnum1-100, wnum2=lblwnum2+100, tape5=tp5+'.99', 
+             mlayers=mlayerz, wnum1=lblwnum1-100, wnum2=lblwnum2+100, tape5=tp5+'.99',
              v10=True, silent=True)
 
         command99 = ('setenv LBL_HOME ' +lblhome + ' ; '+
@@ -1802,8 +1802,8 @@ def compute_jacobian_microwave_finitediff(Xn, p, z, freq, cbh, vip, workdir,
     cth = cbh + 0.300             # km; define the cloud top at x m above the could base
 
         # See the note on sfcz in compute_jacobian_microwavescan_3method
-    if(sfc_alt == None): 
-        sfcz=0 
+    if(sfc_alt == None):
+        sfcz=0
     else:
         sfcz = sfc_alt / 1000.
 
@@ -1914,8 +1914,8 @@ def compute_jacobian_microwave_3method(Xn, p, z, freq, cbh, vip, workdir,
     cth = cbh + 0.300             # km; define the cloud top at x m above the could base
 
         # See the note on sfcz in compute_jacobian_microwavescan_3method
-    if(sfc_alt == None): 
-        sfcz=0 
+    if(sfc_alt == None):
+        sfcz=0
     else:
         sfcz = sfc_alt / 1000.
 
@@ -2530,8 +2530,8 @@ def compute_jacobian_microwavescan_3method(Xn, p, z, mwrscan, cbh, vip, workdir,
         # part of the profile -- I suspect I have a hard coded lower limit in the
         # MONORTM.IN file that I am creating in monortm_v5, but I don't see it...
         # So I am not doing anything about this right now...
-    if(sfc_alt == None): 
-        sfcz=0 
+    if(sfc_alt == None):
+        sfcz=0
     else:
         sfcz = sfc_alt / 1000.
 
@@ -2641,7 +2641,7 @@ def compute_jacobian_microwavescan_3method(Xn, p, z, mwrscan, cbh, vip, workdir,
         # Capture the different optical depths into simple matrices
         if(didfail == 1):
             if(verbose >= 1):
-                print('    Bending angle problem at {:6.3f} degree elevation angle in mwrScan'.format(uelev(ii)))
+                print('    Bending angle problem at {:6.3f} degree elevation angle in mwrScan'.format(uelev[ii]))
         else:   # code did not fail
             od0 = np.copy(a['od'].T)
             if fixt != 1:

TROPoe.py
@@ -32,7 +32,7 @@ if '--vip' in sys.argv:
 
     sys.exit()
 
-#Create parser for command line arguments
+# Create parser for command line arguments
 parser = ArgumentParser()
 
 parser.add_argument("date", type=int, help="Date to run the code [YYYYMMDD]")
@@ -1516,7 +1516,7 @@ for i in range(len(aeri['secs'])):                        # { loop_i
         # in-situ surface co2 measurements of the observation vector
         foo = np.where(flagY == 9)[0]
         if len(foo) > 0:
-            flag, KK, FF = Jacobian_Functions.compute_jacobian_external_sfc_co2(Xn, p, z, KK, FF,
+            flag, KK, FF = Jacobian_Functions.compute_jacobian_external_sfc_co2(Xn, p, z,
                         ext_tseries['co2_sfc_relative_height'], vip['retrieve_co2'],
                         vip['fix_co2_shape'], ext_tseries['nptsCO2'])
 