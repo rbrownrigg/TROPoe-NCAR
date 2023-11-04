Updated code to fix the "append bug".  This required storing the prior mean/covariance as doubles in output netCDF file, modifying create_xret to read in and pass back the prior info and surface pressure from the first profile, and some logic to handle those variables properly.  Also fixed a bug with the averaging kernal the does not include the model data (trapping for a condition)

Output_Functions.py
@@ -667,11 +667,11 @@ def write_output(vip, ext_prof, mod_prof, ext_tseries, globatt, xret, prior,
                        + '(5) ice cloud optical depth, (6) ice cloud Reff, (7) carbon dioxide' 
                        + ' (8) methane, (9) nitrous oxide')
 
-        Xa = fid.createVariable('Xa', 'f4', ('arb_dim1',))
+        Xa = fid.createVariable('Xa', 'f8', ('arb_dim1',))
         Xa.long_name = 'Prior mean state'
         Xa.comment1 = 'mixed units -- see field arb above'
 
-        Sa = fid.createVariable('Sa', 'f4', ('arb_dim1','arb_dim2',))
+        Sa = fid.createVariable('Sa', 'f8', ('arb_dim1','arb_dim2',))
         Sa.long_name = 'Prior covariance'
         Sa.comment1 = 'mixed units -- see field arb above'
         
@@ -1005,7 +1005,10 @@ def write_output(vip, ext_prof, mod_prof, ext_tseries, globatt, xret, prior,
 # "secs" field needs to have the proper values though...
 ################################################################################
 
-def create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, obsdim, obsflag,shour):
+def create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, obsdim, obsflag, shour):
+
+    # Set a default value
+    opres = -8888.  # Special flag, to help with the debugging if the pressure is negative
 
     # Find all of the output files with this date
     yy = np.array([datetime.utcfromtimestamp(x).year for x in irs['secs']])
@@ -1023,10 +1026,10 @@ def create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, obsdim, obsflag,shour
             print('The flag output_clobber was set to 2 for append, but no prior file was found')
             print('    so code will run as normal')
             nfilename = ' '
-            return xret, fsample, nfilename
+            return xret, fsample, nfilename, Xa, Sa, opres
         else:
             nfilename = ' '
-            return xret, fsample, nfilename
+            return xret, fsample, nfilename, Xa, Sa, opres
     
     # Check to see if a file with the same shour exists
     found = False
@@ -1043,19 +1046,19 @@ def create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, obsdim, obsflag,shour
         print('     to prevent clobbering.')
         print('     Existing file name: ' + files[i])
         nfilename = files[i]
-        return xret, -1, nfilename
+        return xret, -1, nfilename, Xa, Sa, opres
     
     # There was a file for this day, but there were different shours
     if not found and vip['output_clobber'] == 0:
         nfilename = ' '
-        return xret, fsample, nfilename
+        return xret, fsample, nfilename, Xa, Sa, opres
     
     if not found and vip['output_clobber'] == 2:
         print('The flag output_clobber was set to 2 for append, but no prior file')
         print('      with the same shour was found so code will run as normal')
         print('      and create a new file.')
         nfilename = ' '
-        return xret, fsample, nfilename
+        return xret, fsample, nfilename, Xa, Sa, opres
         
     if not found:
         print('Error: There seems to be some condition that was unanticipated -- aborting')
@@ -1068,12 +1071,29 @@ def create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, obsdim, obsflag,shour
     to = fid.variables['time_offset'][:]
     xobsdim = fid.variables['obs_dimension'][:]
     xobsflag = fid.variables['obs_flag'][:]
+    pressure = fid.variables['pressure'][:]
     xz = fid.variables['height'][:]
     xXa = fid.variables['Xa'][:]
     xSa = fid.variables['Sa'][:]
     fid.close()
 
     secs = bt+to
+
+    # Convert the masked arrays to ndarrays
+    #   Note that setting the mask values to zero should not do anything, as there shouldn't
+    # be any NaNs in the prior....
+    fooXa = np.where(xXa.mask != 0)[0]
+    fooSa = np.where(xSa.mask != 0)[0]
+    if((len(fooXa) > 0) or (len(fooSa) > 0)):
+        print('The mean prior or its covariance, which is coming from a preexisting output file, has NaNs. Aborting retrieval')
+        nfilename = files[i]
+        return xret, -1, nfilename, Xa, Sa, opres
+    oXa = xXa.filled(fill_value=np.double(-9999))
+    oSa = xSa.filled(fill_value=np.double(-9999))
+
+    # We want to extract out the surface pressure for the first sample also, as this
+    # is needed to build the pressure profile if VIP.first_guess=1 in append mode
+    opres = pressure[0,0]
 
     # Set up some default values
     w0idx, nY = Other_functions.find_wnum_idx(irs['wnum'],bands)
@@ -1096,29 +1116,21 @@ def create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, obsdim, obsflag,shour
     if ((len(xz) != len(z)) | (len(foo) > 0)):
         print('Error: output_clobber is set to 2 (append), but there is a mismatch in heights')
         fsample = -1
-        return xret, fsample, nfilename
-
-    diff = np.abs(Xa-xXa)
-
-    foo = np.where(diff > 0.001)[0]
-    if ((len(Xa) != len(xXa)) | (len(foo) > 0)):
-        print('Error: output_clobber is set to 2 (append), but there is a mismatch in Xa')
-        fsample = -1
-        return xret, fsample, nfilename
+        return xret, fsample, nfilename, Xa, Sa, opres
 
     diff = np.abs(obsdim - xobsdim)
     foo = np.where(diff > 0.001)[0]
     if ((len(obsdim) != len(xobsdim)) | (len(foo) > 0)):
         print('Error: output_clobber is set to 2 (append), but there is a mismatch in obs_dim')
         fsample = -1
-        return xret, fsample, nfilename
+        return xret, fsample, nfilename, Xa, Sa, opres
 
     diff = np.abs(obsflag - xobsflag)
     foo = np.where(diff > 0.001)[0]
     if ((len(obsflag) != len(xobsflag)) | (len(foo) > 0)):
         print('Error: output_clobber is set to 2 (append), but there is a mismatch in obs_flag')
         fsample = -1
-        return xret, fsample, nfilename
+        return xret, fsample, nfilename, Xa, Sa, opres
 
     # This structure must match that at the end of the main iteration loop
     # where the retrieval is performed. The values can be anything, with
@@ -1144,4 +1156,76 @@ def create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, obsdim, obsflag,shour
         xret[i]['secs'] = secs[i]
     fsample = len(secs)
 
-    return xret, fsample, nfilename
+    return xret, fsample, nfilename, oXa, oSa, opres
+
+################################################################################
+# This function writes out the prior data into a temporary netCDF file (used for debugging)
+################################################################################
+
+def write_XaSa(filename, nht, X0, Xa, Sa):
+    print(f'  DEBUG -- Creating the file {filename:s}')
+
+    fid = Dataset(filename, 'w')
+
+    adim1 = fid.createDimension('arb_dim1', len(X0))
+    adim2 = fid.createDimension('arb_dim2', len(X0))
+
+    arb1 = fid.createVariable('arb1', 'i2', ('arb_dim1',))
+    arb1.long_name = 'Arbitrary dimension'
+    arb1.comment1 = 'mixed units'
+    arb1.comment2 = ('contains (1) temperature profile, (2) water vapor profile'
+                       + ' (3) liquid water path, (4) liquid water Reff, '
+                       + '(5) ice cloud optical depth, (6) ice cloud Reff, (7) carbon dioxide'
+                       + ' (8) methane, (9) nitrous oxide')
+
+    arb2 = fid.createVariable('arb2', 'i2', ('arb_dim2',))
+    arb2.long_name = 'Arbitrary dimension'
+    arb2.comment1 = 'mixed units'
+    arb2.comment2 = ('contains (1) temperature profile, (2) water vapor profile,'
+                       + ' (3) liquid water path, (4) liquid water Reff, '
+                       + '(5) ice cloud optical depth, (6) ice cloud Reff, (7) carbon dioxide,'
+                       + ' (8) methane, (9) nitrous oxide')
+
+    xXa = fid.createVariable('Xa', 'f8', ('arb_dim1',))
+    xXa.long_name = 'Prior mean state'
+    xXa.comment1 = 'mixed units -- see field arb above'
+
+    xSa = fid.createVariable('Sa', 'f8', ('arb_dim1','arb_dim2',))
+    xSa.long_name = 'Prior covariance'
+    xSa.comment1 = 'mixed units -- see field arb above'
+        
+    fid.dataset_comment = 'File containing the prior information'
+
+    ones = np.ones(nht)
+    twos = np.ones(nht)*2
+    tmp  = np.append(ones,twos)
+    tmp  = np.append(tmp, np.array([3,4,5,6,7,7,7,8,8,8,9,9,9]))
+
+    arb1[:]  = tmp
+    arb2[:]  = tmp
+    xXa[:]   = Xa
+    xSa[:,:] = Sa
+
+    fid.close()
+    return
+
+################################################################################
+# This function reads in the prior data into a temporary netCDF file (used for debugging)
+################################################################################
+
+def read_XaSa(filename):
+    print(f'  DEBUG -- Reading the file {filename:s}')
+
+    fid = Dataset(filename, 'r')
+    xXa = fid.variables['Xa'][:]
+    xSa = fid.variables['Sa'][:]
+    fid.close()
+
+    xXa.mask = xXa.mask * 0
+    xSa.mask = xSa.mask * 0
+    oXa = xXa.filled(fill_value=np.double(-9999)).astype('float64')
+    oSa = xSa.filled(fill_value=np.double(-9999)).astype('float64')
+    oXa = xXa.filled(fill_value=np.double(-9999))
+    oSa = xSa.filled(fill_value=np.double(-9999))
+
+    return oXa, oSa

TROPoe.py
@@ -11,7 +11,7 @@
 #
 # ----------------------------------------------------------------------------
 
-__version__ = '0.10.5'
+__version__ = '0.10.6'
 
 import os
 import sys
@@ -652,7 +652,7 @@ if vip['recenter_prior'] > 0:
         recenter_input_value = vip['recenter_input']
     elif ((vip['recenter_prior'] == 1) | (vip['recenter_prior'] == 3) | (vip['recenter_prior'] == 5)):
         if ((vip['ext_sfc_wv_type'] > 0) & (ext_tseries['nQsfc'] > 0) & (vip['recenter_prior'] != 5)):  # Rescale based on the ext_sfc data
-            foo = np.where(ext_tseries['wv'] > 0)  # Need to take out an -999s
+            foo = np.where(ext_tseries['wv'] > 0)[0]  # Need to take out an -999s
             recenter_input_value = np.mean(ext_tseries['wv'][foo])
         else:
             if vip['recenter_prior'] != 5:
@@ -1167,7 +1167,7 @@ for i in range(len(irs['secs'])):                        # { loop_i
     # If we are to append to the file, then I need to find the last valid
     # sample in the file, so I only process after that point...
     if ((vip['output_clobber'] == 2) & (check_clobber == 1)):
-        xret, fsample, noutfilename = Output_Functions.create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, dimY, flagY, shour)
+        xret, fsample, noutfilename, origXa, origSa, opres = Output_Functions.create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, dimY, flagY, shour)
         check_clobber = 0
         if fsample < 0:
             VIP_Databases_functions.abort(lbltmpdir,date)
@@ -1177,12 +1177,14 @@ for i in range(len(irs['secs'])):                        # { loop_i
             xret = []
         if ((verbose >= 1) & (fsample > 0)):
             print(('  Will append output to the file ' + noutfilename))
-            print(f'    Starting with sample {fsample:4d}')
-    
+            print(f'    Starting with sample {fsample:4d} and using the original prior data')
+            Xa = origXa
+            Sa = origSa
+
     # If we are not in append mode, but do not have clobber set to 1 then
     # check to see if a conflicting file exists and if so abort
     elif ((vip['output_clobber'] == 0) & (check_clobber == 1)):
-        xret, fsample, noutfilename = Output_Functions.create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, dimY, flagY, shour)
+        xret, fsample, noutfilename, origXa, origSa, opres = Output_Functions.create_xret(xret, fsample, vip, irs, Xa, Sa, z, bands, dimY, flagY, shour)
         check_clobber = 0
         if fsample < 0:
            VIP_Databases_functions.abort(lbltmpdir,date)
@@ -1244,8 +1246,19 @@ for i in range(len(irs['secs'])):                        # { loop_i
         if os.path.exists(lbllog):
             shutil.rmtree(lbllog)
 
-        # Update the pressure profile using the current estimate of temperature
-        p = Calcs_Conversions.inv_hypsometric(z, Xn[0:int(nX/2)]+273.16, irs['atmos_pres'][i])
+        # Update the pressure profile using the current estimate of temperature.  However, if this is
+        # the first iteration, we need to know what is being used for the first guess; if we are using 
+        # the prior, then we are going to use a canned Jacobian for that first iteration, which means
+        # we have to use the same pressure profile (which means the same surface pressure value)
+        if((itern == 0) and (vip['first_guess'] == 1) & (opres > 0)):
+            sfcp = opres
+        else:
+            sfcp = irs['atmos_pres'][i]
+        if sfcp <= 0:
+            print(f'  Error: the surface pressure is not positive (sfcP = {sfcp:f}); aborting')
+            VIP_Databases_functions.abort(lbltmpdir,date)
+            sys.exit()
+        p = Calcs_Conversions.inv_hypsometric(z, Xn[0:int(nX/2)]+273.16, sfcp)
 
         # If the trace gas profile shape is mandated to be a function of the PBL height,
         # then set that here. First, compute the current estimate of the PBL height,
@@ -1763,15 +1776,16 @@ for i in range(len(irs['secs'])):                        # { loop_i
         
         # Calculate the Akern without model data included
         foo = np.where((flagY<7) | (flagY>8))[0]
-
-        tmp_Kij = np.copy(Kij[foo,:])
-        tmp_Sm = np.copy(Sm[foo,:])
-        tmp_Sm = tmp_Sm[:,foo]
-        tmp_SmInv = scipy.linalg.pinv(tmp_Sm)
-        tmp_B = (gfac * SaInv) + tmp_Kij.T.dot(tmp_SmInv).dot(tmp_Kij)
-        tmp_Binv = scipy.linalg.pinv(tmp_B)
-        Akern_nm = (tmp_Binv.dot(tmp_Kij.T).dot(tmp_SmInv).dot(tmp_Kij)).T
-        
+        if len(foo) > 0:
+            tmp_Kij = np.copy(Kij[foo,:])
+            tmp_Sm = np.copy(Sm[foo,:])
+            tmp_Sm = tmp_Sm[:,foo]
+            tmp_SmInv = scipy.linalg.pinv(tmp_Sm)
+            tmp_B = (gfac * SaInv) + tmp_Kij.T.dot(tmp_SmInv).dot(tmp_Kij)
+            tmp_Binv = scipy.linalg.pinv(tmp_B)
+            Akern_nm = (tmp_Binv.dot(tmp_Kij.T).dot(tmp_SmInv).dot(tmp_Kij)).T
+        else:
+            Akern_nm = np.copy(Akern) * 0 - 999.
         
         if(vip['max_iterations'] == 0):
             if(vip['irs_type'] > 0):